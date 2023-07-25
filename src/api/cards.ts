e057b7148abd559b03136fe17ff46dc9a6599afe
Maciej Petrykowski <maciej.petrykowski@itboom.pl>
2023-07-25 10:38:32 +0200

    adding cards

--- src/api/cards.ts
+++ src/api/cards.ts
@@ -6,11 +6,30 @@ const router = express.Router();
 export interface Card {
   content: string;
   person: string;
-  id: number;
+  id?: number;
 }
 
 router.get<{}, Card[]>("/", (req, res) => {
   res.json(cardsData);
+});
+router.post<Card, any>("/", (req, res) => {
+  const ids: number[] = cardsData.map((x) => x.id) as number[];
+  let newId = Math.max(...ids) + 1;
+
+  let newCard: Card = {
+    content: req.body.content,
+    person: req.body.person,
+    id: newId,
+  };
+  if (!newCard.content || !newCard.person) {
+    res.status(400);
+    res.send("bad request");
+    return;
+  }
+
+  cardsData.push(newCard);
+
+  res.json(newId);
 });
 
 export default router;
