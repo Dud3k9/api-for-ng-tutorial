87775ad89051772b239cf8a8d5484de39abe8104
Maciej Petrykowski <maciej.petrykowski@itboom.pl>
2023-07-25 15:36:59 +0200

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
 
-import * as middlewares from './middlewares';
-import api from './api';
+import * as middlewares from "./middlewares";
+import api from "./api";
+import cors from "cors";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-// app.use(morgan('dev'));
-// app.use(helmet());
+app.use(cors({ origin: "*" }));
+app.use(morgan("dev"));
+app.use(helmet());
 app.use(express.json());
 
-
-
-app.use('/api/', api);
+app.use("/api/", api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
